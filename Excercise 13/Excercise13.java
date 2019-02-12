4d3188538d1ff09967859bf9d2691ce1cb0617df
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-12 12:26:50 -0700

    Update Excercise13.java

--- Excercise 13/Excercise13.java
+++ Excercise 13/Excercise13.java
@@ -3,12 +3,14 @@
 class Excercise13{
 	public static void main(String[] args){
 		
-		double width = 5.5;
-		double height = 8.5;		
-		double area = width * height;
-		double perimeter = 2 * (width + height);
+		double width = 5.5; //Se crea una variable width de tipo double que se inicializa con un valor de 5.5.
+		double height = 8.5; //Se crea una variable height de tipo double que se inicializa con un valor de 8.5
+		double area = width * height; // Se crea una variable area de tipo double que se inicizaliza con el valor 
+						//de la operacion width * height. Como resultado debe dar 46.75. 
 		
-		System.out.println("Area is " + width + " * " + height + " = " + area);
+		double perimeter = 2 * (width + height); //Se crea una variable perimeter de tipo double que se inicializa con el valor de 
+			
+		System.out.println("Area is " + width + " * " + height + " = " + area); // Se 
 		System.out.println("Perimeter is 2 * (  " + width + " + " + height + ") = " + perimeter);
 
 	}
