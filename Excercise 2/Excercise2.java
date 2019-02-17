38cd496687376d553d0d2799be54ab86a970e11d
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 12:32:41 -0700

    Update Excercise2.java

--- Excercise 2/Excercise2.java
+++ Excercise 2/Excercise2.java
@@ -1,7 +1,15 @@
 //Edgar Omar Reyes Oivas 12:39
 
-class Excercise2{
-	public static void main(String[] args){
-		System.out.println(74+36);	
-	}
-}
+class Excercise2{ //Declaration of a class named Excercise2
+	
+	public static void main(String[] args){ //Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it.
+
+		System.out.println(74+36); 	//Printing the addition of two numbers. System it's a class. Out it's a field on the system class, 
+						//it's an standard output stream. Println() it's a method of the PrintStream class 
+						//which prints any type of primitive data types and then terminates the line.
+	} //main method closing bracket
+} //Excercise2 class closing bracket
