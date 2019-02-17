a025d59ca371fb2563c14c5750bb4cb836563788
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 12:30:28 -0700

    Update Excercise1.java

--- Excercise 1/Excercise1.java
+++ Excercise 1/Excercise1.java
@@ -1,8 +1,14 @@
 //Edgar Omar Reyes Olivas	12:26		02/05/2019
 
-class Exercise1 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-	System.out.println("Edgar Omar Reyes Olivas");
-    }
-}
+class Exercise1 { //Declaration of a class named Excercise1
+    public static void main(String[] args) { 	//Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it. 
+	    
+	System.out.println("Edgar Omar Reyes Olivas"); 	//Printing my name. System it's a class. Out it's a field on the system class, 
+	    						//it's an standard output stream. Println() it's a method of the PrintStream class 
+	    						//which prints any type of primitive data types and then terminates the line.
+    } //main method closing bracket
+} //Excersie1 class closing bracket
