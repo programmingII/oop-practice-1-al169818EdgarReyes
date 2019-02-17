ea9db586566cf81240fcaf961fa7ba4506e42265
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 12:36:17 -0700

    Update Excercise3.java

--- Excercise 3/Excercise3.java
+++ Excercise 3/Excercise3.java
@@ -1,8 +1,14 @@
-//Edgar Omar Reyes Olivas 12:47
+//Nombre: Edgar Omar Reyes Olivas 		Dia: 02/05/19		Hora: 12:47
 
-class Excercise3{
-	public static void main(String[] args){
+class Excercise3{ //Declaration of a class named Excercise3
+	public static void main(String[] args){ //Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it. 
 		
-		System.out.println(50/3);
-	}
-}
+		System.out.println(50/3); 	//Printing the division between two numbers. System it's a class. Out it's a field on the system class, 
+	    					//it's an standard output stream. Println() it's a method of the PrintStream class 
+	    					//which prints any type of primitive data types and then terminates the line.
+	} //main closing bracket
+} //class closing bracket
