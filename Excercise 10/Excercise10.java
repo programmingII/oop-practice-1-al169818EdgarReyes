fa0ed249c1a809b7b1410ce3ff382444c2e102f3
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 14:54:32 -0700

    Update Excercise10.java

--- Excercise 10/Excercise10.java
+++ Excercise 10/Excercise10.java
@@ -1,9 +1,17 @@
 //Edgar Omar Reyes Olivas		07/02/2019	12:50
 
-class Excercise10{
-	public static void main(String[] args){
+class Excercise10{ //Declaration of a class named Excercise10
+	public static void main(String[] args){ //Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it.
 
-		System.out.println("The formula's result is: " + 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));		
+		System.out.println("The formula's result is: " + 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));	
+		////Prints "The formula's result is: " followed by the result of the formula. System it's a class. Out it's a field on the system class, 
+		//it's an standard output stream. Println() it's a method of the PrintStream class 
+		//which prints any type of primitive data types and then terminates the line.
+		
 
 	}
 }
