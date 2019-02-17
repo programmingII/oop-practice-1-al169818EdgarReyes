4ad3ee9d5ab049db2d7d0c7d36cbb0ac08874f3c
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 14:43:52 -0700

    Update Excercise9.java

--- Excercise 9/Excercise9.java
+++ Excercise 9/Excercise9.java
@@ -1,7 +1,15 @@
-//Edgar Omar Reyes Olivas 		02/07/2019	12:48
+//Nombre: Edgar Omar Reyes Olivas 		Fecha: 02/07/2019	Hora: 12:48
 
-class Excercise9{
-	public static void main(String[] args){
-		System.out.println("Result of ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) = " + ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) );
+class Excercise9{ //Declaration of a class named Excercise9
+	public static void main(String[] args){	//Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it.
+		
+		System.out.println("Result of ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) = " + ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) ); 
+		////Prints the text result of followed by the result of the mathematical operations . System it's a class. Out it's a field on the system class, 
+		//it's an standard output stream. Println() it's a method of the PrintStream class 
+		//which prints any type of primitive data types and then terminates the line.
 	}	
 }
