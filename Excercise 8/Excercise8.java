f8f931bab0230fa4743ce638b484079628a2331d
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 14:37:49 -0700

    Update Excercise8.java

--- Excercise 8/Excercise8.java
+++ Excercise 8/Excercise8.java
@@ -1,13 +1,19 @@
-//Edgar Omar Reyes Olivas		2/7/2019	12:38
+//Nombre: Edgar Omar Reyes Olivas		Fecha: 2/7/2019			Hora: 12:38
 
-class Excercise8{
+class Excercise8{ //Declaration of a class named Excercise8
 
-	public static void main(String[] args){
+	public static void main(String[] args){ //Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it.
 
-		System.out.println("   J    a   v     v  a"   );	
-		System.out.println("   J   a a   v   v  a a");
-		System.out.println("J  J  aaaaa   V V  aaaaa");
-		System.out.println(" JJ  a     a   V  a     a ");
+		System.out.println("   J    a   v     v  a"   ); //Prints  J    a   v     v  a. System it's a class. Out it's a field on the system class, 
+								 //it's an standard output stream. Println() it's a method of the PrintStream class 
+								 //which prints any type of primitive data types and then terminates the line.
+		System.out.println("   J   a a   v   v  a a");	//Prints  J   a a   v   v  a a
+		System.out.println("J  J  aaaaa   V V  aaaaa"); //Prints  J  J  aaaaa   V V  aaaaa
+		System.out.println(" JJ  a     a   V  a     a "); //Prints  JJ  a     a   V  a     a 
 
 	}
 
