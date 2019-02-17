bc0fef22f37ac9f7e90e9f1157dc5143d29e1ede
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 14:11:15 -0700

    Update Excercise6.java

--- Excercise 6/Excercise6.java
+++ Excercise 6/Excercise6.java
@@ -1,22 +1,47 @@
 //Edgar Omar Reyes Olivas 13:17
 
-import java.util.Scanner;
+import java.util.Scanner; //On this line I include the external class Scanner.
 
-class Excercise6{
-	public static void main(String[] args){
-				
-		Scanner input = new Scanner(System.in);
+class Excercise6{ //Declaration of a class named Excercise6
+	public static void main(String[] args){ //Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it.
 		
-		System.out.println("Input first number: ");
-		int x = input.nextInt();
-		System.out.println("Input second number");
-		int y = input.nextInt();
+		Scanner input = new Scanner(System.in); // Declaration of an object named input of the class Scanner. Then with the new 
+							//operator we create an object and call the constructor that is Scanner. Finally
+							//we initialize the object with the argument System.in which is an input stream.
+		
+		System.out.println("Input first number: "); 	//Prints Input first number. System it's a class. Out it's a field on the system class, 
+								//it's an standard output stream. Println() it's a method of the PrintStream class 
+								//which prints any type of primitive data types and then terminates the line.
+		
+		int x = input.nextInt();			//Initialization of the variable x which value is the scan of the next 
+								//token of the input as an int. Input is the name of the object, the dot 
+								//operator is used to call the methods of the class and we leave empty 
+								//parenthesis becasue there are no arguments to fill with.
+		
+		System.out.println("Input second number");	//Prints Input second number.
+		int y = input.nextInt();			//Initialization of the variable y which value is the scan of the next 
+								//token of the input as an int. Input is the name of the object, the dot 
+								//operator is used to call the methods of the class and we leave empty 
+								//parenthesis becasue there are no arguments to fill with.
 
-		System.out.println("Addition : " + (x + y));
-		System.out.println("Multiplication: " + (x * y));
-		System.out.println("Substraction: " + (x - y));
-		System.out.println("Division: " + (x / y));
-		System.out.println("Remainder: " + (x % y));
+		System.out.println("Addition : " + (x + y));	//Prints the text addition followed by the addition of x and y. + is used for
+								//concatenation.
+		
+		System.out.println("Multiplication: " + (x * y)); //Prints the text Multiplication followed by the Product of x and y. + is used for
+								//concatenation.
+		
+		System.out.println("Substraction: " + (x - y)); //Prints the text Substraction followed by the substractionn of x and y. + is used for
+								//concatenation.
+		
+		System.out.println("Division: " + (x / y)); 	//Prints the text division followed by the division of x and y. + is used for
+								//concatenation.
+		
+		System.out.println("Remainder: " + (x % y)); 	//Prints the text remainder followed by the remainder of x and y. + is used for
+								//concatenation.
 
 	}
 
