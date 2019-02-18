32a0498abdafae1dad643e85c3649c2048898e25
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 17:48:09 -0700

    Update Excercise16.java

--- Excercise 16/Excercise16.java
+++ Excercise 16/Excercise16.java
@@ -3,11 +3,14 @@ class Excercise16{
 
 	public static void main(String[] args){
 	
-	System.out.println(" +\"\"\"\"\"+ ");
-	System.out.println("[| o o |]");
-	System.out.println(" |  ^  |");
-	System.out.println(" | '_' |");
-	System.out.println(" +-----+");
+	System.out.println(" +\"\"\"\"\"+ "); //Prints " +\"\"\"\"\"+ " . 
+						//System it's a class. Out it's a field on the system class, 
+						//it's an standard output stream. Println() it's a method of the PrintStream class 
+						//which prints any type of primitive data types and then terminates the line.
+	System.out.println("[| o o |]"); //Prints"[| o o |]". 
+	System.out.println(" |  ^  |");	//Prints " |  ^  |".
+	System.out.println(" | '_' |"); //Prints " | '_' |"
+	System.out.println(" +-----+"); //Prints  "+-----+"
 
 	}
 }
