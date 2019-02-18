2d03b899579f3c63ade5cb840e4b40f1809609a0
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-02-17 17:32:53 -0700

    Update Excercise14.java

--- Excercise 14/Excercise14.java
+++ Excercise 14/Excercise14.java
@@ -1,25 +1,32 @@
 //Nombre:Edgar Omar Reyes Olivas 	Dia: 02/12/2019		Hora:12:48
 
-class Excercise14{
+class Excercise14{//Declaration of class named Excercise14
 
-	public static void main(String[] args){
+	public static void main(String[] args){ //Declaration of the main function. Public it's an access specifier that means that you can 
+	    					//call this method from outside the class (publically). Static it's an access modifier that 
+	    					//creates space in memory automatically when java program is load. Void it's a return type.
+	    					//Main it's the name of the method. String[] args are arguments of type String that your Java 
+	    					//application accepts when you run it.
 
 	
-	System.out.println("* * * * * * ===========================");
-	System.out.println(" * * * * *  =========================== ");
-	System.out.println("* * * * * * ===========================");
-	System.out.println(" * * * * *  ===========================");
-	System.out.println("* * * * * * ===========================");
-	System.out.println(" * * * * *  =========================== ");
-	System.out.println("* * * * * * ===========================");
-	System.out.println(" * * * * *  ===========================");	
-	System.out.println("* * * * * * ===========================");
-	System.out.println("=======================================");
-	System.out.println("=======================================");
-	System.out.println("=======================================");
-	System.out.println("=======================================");
-	System.out.println("=======================================");
-	System.out.println("=======================================");
+	System.out.println("* * * * * * ===========================");     //Prints "* * * * * * ===========================". 
+									  //System it's a class. Out it's a field on the system class, 
+								         //it's an standard output stream. Println() it's a method of the PrintStream class 
+						                        //which prints any type of primitive data types and then terminates the line.
+	System.out.println(" * * * * *  =========================== "); //Prints "* * * * * * ===========================". 
+	System.out.println("* * * * * * ==========================="); //Prints "* * * * * * ===========================". 
+	System.out.println(" * * * * *  ==========================="); //Prints "* * * * * * ===========================". 
+	System.out.println("* * * * * * ==========================="); //Prints "* * * * * * ===========================". 
+	System.out.println(" * * * * *  =========================== ");//Prints "* * * * * * ===========================". 
+	System.out.println("* * * * * * ==========================="); //Prints "* * * * * * ===========================". 
+	System.out.println(" * * * * *  ==========================="); //Prints "* * * * * * ===========================". 
+	System.out.println("* * * * * * ==========================="); //Prints "* * * * * * ===========================". 
+	System.out.println("======================================="); //Prints "=======================================". 
+	System.out.println("=======================================");//Prints "=======================================". 
+	System.out.println("=======================================");//Prints "=======================================". 
+	System.out.println("=======================================");//Prints "=======================================". 
+	System.out.println("=======================================");//Prints "=======================================". 
+	System.out.println("=======================================");//Prints "=======================================". 
 	
 	}
 }
